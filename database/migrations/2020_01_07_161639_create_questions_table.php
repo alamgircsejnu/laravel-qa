78a45cd646bec783f157b40ebca4c7221605787d
Alamgir Hossain <alamgircsejnu@gmail.com>
2020-01-07 23:27:21 +0600

    prepopulate users and questions tables using model factories and seed

--- database/migrations/2020_01_07_161639_create_questions_table.php
+++ database/migrations/2020_01_07_161639_create_questions_table.php
@@ -19,9 +19,10 @@ class CreateQuestionsTable extends Migration
             $table->string('slug')->unique();
             $table->text('body');
             $table->integer('views')->unsigned()->default(0);
+            $table->integer('answers')->unsigned()->default(0);
             $table->integer('votes')->default(0);
             $table->integer('best_answer_id')->unsigned()->nullable();
-            $table->integer('user_id')->unsigned()->nullable();
+            $table->bigInteger('user_id')->unsigned()->index()->nullable();
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');

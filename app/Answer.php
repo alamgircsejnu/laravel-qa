eddd016fbca2b6ce8872d8911423b4e6504c7b7f
Alamgir Hossain <alamgircsejnu@gmail.com>
2020-01-13 20:46:27 +0600

    Make the vote answer working

--- app/Answer.php
+++ app/Answer.php
@@ -56,5 +56,16 @@ class Answer extends Model
         return $this->morphToMany(User::class, 'votable')->withTimestamps();
     }
 
+    public function upVotes()
+    {
+        return $this->votes()->wherePivot('vote', 1);
+    }
+
+
+    public function downVotes()
+    {
+        return $this->votes()->wherePivot('vote', -1);
+    }
+
 
 }

ce737d21c2a59d91e3deb86b0e57e377d899c07a
Alamgir Hossain <alamgircsejnu@gmail.com>
2020-01-14 15:02:49 +0600

    fontawesome added

--- resources/js/fontawesome.js
+++ resources/js/fontawesome.js
@@ -1,7 +1,1 @@
 import fontawesome from '@fortawesome/fontawesome-free';
-// import faCaretUp from '@fortawesome/fontawesome-free/faCaretUp';
-// import faCaretDown from '@fortawesome/fontawesome-free/faCaretDown';
-// import faStar from '@fortawesome/fontawesome-free/faStar';
-// import faCheck from '@fortawesome/fontawesome-free/faCheck';
-//
-// fontawesome.library.add([faCaretUp, faCaretDown, faStar, faCheck]);

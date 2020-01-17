af79c15ce675634b4f26cc23ed45547c3426fec4
Alamgir Hossain <alamgircsejnu@gmail.com>
2020-01-18 03:10:54 +0600

    Implement load more answers feature

--- resources/js/components/Answers.vue
+++ resources/js/components/Answers.vue
@@ -9,6 +9,10 @@
                     <hr>
 
                     <answer v-for="answer in answers" :answer="answer" :key="answer.id"></answer>
+
+                    <div class="text-center mt-3" v-if="nextUrl">
+                        <button @click.prevent="fetch(nextUrl)" class="btn btn-outline-secondary">Load more answers</button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -18,16 +22,41 @@
 <script>
 
     import Answer from './Answer.vue';
-    export default {
-        props: ['answers', 'count'],
 
-        computed: {
-            title() {
-                return this.count + " " + (this.count >1 ? "Answers" : "Answer");
+    export default {
+        props: ['question'],
+
+        data() {
+            return {
+                questionId: this.question.id,
+                count: this.question.answers_count,
+                answers: [],
+                nextUrl: null
             }
         },
 
-        components: { Answer }
+        created () {
+          this.fetch(`/questions/${this.questionId}/answers`);
+        },
+
+        methods: {
+          fetch(endpoint) {
+              axios.get(endpoint)
+                  .then(({data}) => {
+                      // console.log(data);
+                      this.answers.push(... data.data);
+                      this.nextUrl = data.next_page_url;
+                  })
+          }
+        },
+
+        computed: {
+            title() {
+                return this.count + " " + (this.count > 1 ? "Answers" : "Answer");
+            }
+        },
+
+        components: {Answer}
     }
 </script>
 
